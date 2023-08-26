40fb4dbce6e1d1ef2bff1e89f7b85814ae55962c
P34RL <undeadgamer279@gmail.com>
2023-08-26 01:40:38 -0400

    fixed code

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,20 @@
-mod files {
-    include!(concat!(env!("OUT_DIR"), "/generated_files.rs"));
+#[macro_use]
+extern crate rocket;
+
+use rocket::response::{content::RawHtml, status::NotFound};
+
+use include_dir::{include_dir, Dir};
+static PROJECT_DIR: Dir = include_dir!("dist");
+
+#[get("/<path>")]
+fn files(path: &str) -> Result<RawHtml<&str>, NotFound<&str>> {
+    match PROJECT_DIR.get_file(path) {
+        Some(file) => Ok(RawHtml(file.contents_utf8().unwrap())),
+        None => Err(NotFound("Couldn't Find File")),
+    }
 }
 
-fn main() {
-    println!("{:?}", files::INDEX_HTML);
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![files])
 }
